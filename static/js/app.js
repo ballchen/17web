7b4837bf86253359654ed6307fea014cfe0fb7b8
ballchen <kerkerball@gmail.com>
2015-12-15 00:44:31 +0800

    no need to download json

--- static/js/app.js
+++ static/js/app.js
@@ -3,4 +3,10 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 	$http.get('/collection').success(function(data){
 		$scope.collections = data;
 	})
+
+	$scope.getCollection = function(name){
+		$http.get('/hots/'+name).success(function(data){
+			$scope.current = data;
+		})
+	}
 }])
