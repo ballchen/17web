459467cbf0dd0c04c40dee7dee0dcc8a1bb77057
ballchen <kerkerball@gmail.com>
2015-12-15 23:46:18 +0800

    show hide

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,20 @@
 var app = angular.module('MyApp', []);
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.showList = null;
 	$http.get('/collection').success(function(data){
-		$scope.collections = data;
+		$scope.collections = _.groupBy(data, function(n){
+			return n.split('-')[0];
+		})	
+		console.log($scope.collections)
 	})
 
 	$scope.getCollection = function(name){
 		$http.get('/hots/'+name).success(function(data){
-			$scope.current = data;
+			$scope.current = data
 		})
+	}
+
+	$scope.Show = function(i){
+		$scope.showList = i;
 	}
 }])
